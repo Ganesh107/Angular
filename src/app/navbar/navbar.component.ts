1ee60ae374e03262258d8f48d50cbb764657b75e
Ganesh107 <sganesh371@gmail.com>
2025-03-25 00:49:48 +0530

    added routing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
+  styles: [`
+    a.active {
+      font-weight: bold;
+    }
+  `]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  constructor(private router: Router){ }
 
+  ngOnInit(): void {
+  }
 }
