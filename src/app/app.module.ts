2f89daf78384d93d0d2a20bdde3a4b6820a858e1
Ganesh107 <sganesh371@gmail.com>
2025-04-07 00:17:43 +0530

    contact component updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
@@ -12,6 +12,7 @@ import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { authInterceptor } from './auth.interceptor';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { authInterceptor } from './auth.interceptor';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    FormsModule
   ],
   providers: [provideHttpClient(
     withInterceptors([authInterceptor])
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
+  @Input() inputValue:string = "";
 
+  autoComplete(){
+    
+  }
 }
