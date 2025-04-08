34d12180ecadf7b2a2770ebb3173d927d88a001b
Ganesh107 <sganesh371@gmail.com>
2025-04-08 23:46:08 +0530

    debounce added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { debounceTime, timer } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +10,20 @@ export class ContactComponent {
   @Input() inputValue:string = "";
 
   autoComplete(){
-    
+    this.debounce(this.apiRequest, 300)()
+  }
+
+  apiRequest(){
+    console.log('Fetching data from server!!!')
+  }
+
+  debounce(func: any, delay: any){
+    let timerId: any;
+    return (...args: any) => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        func.apply(this, args)
+      }, delay)
+    }
   }
 }
