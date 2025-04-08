31846c680694ca589232bcbc21b456b432cca955
Ganesh107 <sganesh371@gmail.com>
2025-04-09 00:05:29 +0530

    debounce updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,29 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { debounceTime, timer } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: false,
   templateUrl: './contact.component.html',
 })
-export class ContactComponent {
-  @Input() inputValue:string = "";
+export class ContactComponent implements OnInit {
+  @Input() inputValue: string = "";
 
-  autoComplete(){
-    this.debounce(this.apiRequest, 300)()
+  private debouncedApiRequest!: (...args: any[]) => void;
+
+  ngOnInit() {
+    this.debouncedApiRequest = this.debounce(this.apiRequest, 300);
   }
 
-  apiRequest(){
-    console.log('Fetching data from server!!!')
+  autoComplete() {
+    this.debouncedApiRequest(); 
   }
 
-  debounce(func: any, delay: any){
+  apiRequest() {
+    console.log('Fetching data from server for input: ' + this.inputValue);
+  }
+
+  debounce(func: (...args: any[]) => void, delay: number) {
     let timerId: any;
-    return (...args: any) => {
+    return (...args: any[]) => {
       clearTimeout(timerId);
       timerId = setTimeout(() => {
-        func.apply(this, args)
-      }, delay)
-    }
+        func.apply(this, args);
+      }, delay);
+    };
   }
 }
